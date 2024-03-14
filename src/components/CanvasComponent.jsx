a1256598f59873f49835740db5ae6ef5fb47b072
Emilio <sonarart@gmail.com>
2024-03-13 19:05:43 -0600

    adding basic formatter fn

--- src/components/CanvasComponent.jsx
+++ src/components/CanvasComponent.jsx
@@ -1,5 +1,5 @@
-import { forwardRef } from 'react'
-import { Image } from '@nextui-org/react'
+import { forwardRef, useCallback, useState } from 'react'
+import { Button } from '@nextui-org/react'
 import {
   PiAlienBold,
   PiHardDriveBold,
@@ -19,8 +19,10 @@ import {
 } from 'react-icons/md'
 import { useCanvasBg } from '../context/CanvasBgContext'
 import ReactCodeMirror from '@uiw/react-codemirror'
+import { loadLanguage } from '@uiw/codemirror-extensions-langs'
 import { createTheme } from '@uiw/codemirror-themes'
 import { tags as t } from '@lezer/highlight'
+import stripIndent from 'strip-indent'
 
 const CanvasComponent = forwardRef(
   (
@@ -59,7 +61,7 @@ const CanvasComponent = forwardRef(
       settings: {
         background: '',
         backgroundImage: '',
-        foreground: '#4D4D4C',
+        foreground: '#EAEAEA',
         caret: '#AEAFAD',
         selection: '#D6D6D6',
         selectionMatch: '#D6D6D6',
@@ -78,6 +80,16 @@ const CanvasComponent = forwardRef(
       ],
     })
 
+    const [codeMirrorValue, setCodeMirrorValue] = useState(
+      `console.log('hello world!');`
+    )
+
+    const onCMChange = useCallback((val) => {
+      setCodeMirrorValue(val)
+    }, [])
+
+    const formatCode = () => setCodeMirrorValue(stripIndent(codeMirrorValue))
+
     return (
       <div
         ref={ref}
@@ -86,32 +98,6 @@ const CanvasComponent = forwardRef(
           background: canvasBg?.style,
         }}
       >
-        <div
-          className="bg-content1 p-4"
-          style={{
-            borderRadius: `${borderRadius}px`,
-            transform: `scale(${
-              imgScale * sliderScale
-            }) perspective(1000px) rotateX(${rotationX}deg) rotateY(${rotationY}deg) rotateZ(0deg)`,
-            boxShadow: `rgba(0, 0, 0, 0.6) 0px ${shadow}px ${
-              shadow * 0.9
-            }px 0px`,
-          }}
-        >
-          <ReactCodeMirror
-            value="console.log('hello world!');"
-            theme={myTheme}
-            height="200px"
-            basicSetup={{
-              lineNumbers: false,
-              foldGutter: false,
-              highlightActiveLineGutter: false,
-              highlightSpecialChars: false,
-              allowMultipleSelections: false,
-              highlightActiveLine: false,
-            }}
-          />
-        </div>
         {canvasBg?.imgSrc && (
           <img
             className="absolute top-0 w-full h-full object-cover"
@@ -119,94 +105,118 @@ const CanvasComponent = forwardRef(
             alt="Custom watermark"
           />
         )}
-        {imgSrc && imgVisibility && (
-          <div
-            id="snap-container"
-            className={`flex flex-col overflow-hidden
+        {
+          // imgSrc &&
+          imgVisibility && (
+            <div
+              id="snap-container"
+              className={`flex flex-col overflow-hidden
           `}
-            style={{
-              borderRadius: `${borderRadius}px`,
-              transform: `scale(${
-                imgScale * sliderScale
-              }) perspective(1000px) rotateX(${rotationX}deg) rotateY(${rotationY}deg) rotateZ(0deg)`,
-              boxShadow: `rgba(0, 0, 0, 0.6) 0px ${shadow}px ${
-                shadow * 0.9
-              }px 0px`,
-            }}
-          >
-            {imgFrame === 'macOS-dark' && (
-              <div className="flex items-center justify-between w-full h-8 bg-[#262a35]">
-                <div className="flex gap-2 pl-4">
-                  <div className="flex items-center justify-center w-3 h-3 bg-[#ff5f56] rounded-full"></div>
-                  <div className="flex items-center justify-center w-3 h-3 bg-[#ffbd2e] rounded-full"></div>
-                  <div className="flex items-center justify-center w-3 h-3 bg-[#27c93f] rounded-full"></div>
-                </div>
-              </div>
-            )}
-            {imgFrame === 'macOS-light' && (
-              <div className="flex items-center justify-between w-full h-8 bg-[#f2f5f7]">
-                <div className="flex gap-2 pl-4">
-                  <div className="flex items-center justify-center w-3 h-3 bg-[#ff5f56] rounded-full"></div>
-                  <div className="flex items-center justify-center w-3 h-3 bg-[#ffbd2e] rounded-full"></div>
-                  <div className="flex items-center justify-center w-3 h-3 bg-[#27c93f] rounded-full"></div>
-                </div>
-              </div>
-            )}
-            {imgFrame === 'windows-light' && (
-              <div className="flex items-center justify-end w-full h-8 bg-[#d0dbec]/80 backdrop-blur">
-                <div className="flex gap-2 pr-4">
-                  <MdMinimize className="text-[#313131]" />
-                  <MdOutlineRectangle className="text-[#313131]" />
-                  <MdClose className="text-[#313131]" />
-                </div>
-              </div>
-            )}
-
-            {imgFrame === 'windows-dark' && (
-              <div className="flex items-center justify-end w-full h-8 bg-[#1e1e20]/80 backdrop-blur">
-                <div className="flex gap-2 pr-4">
-                  <MdMinimize className="text-[#ffffff]" />
-                  <MdOutlineRectangle className="text-[#ffffff]" />
-                  <MdClose className="text-[#ffffff]" />
-                </div>
-              </div>
-            )}
-            {imgFrame === 'galaxy' && (
-              // translucent navbar with a purple gradient background
-              <div className="flex items-center justify-between w-full h-8 bg-gradient-to-r from-[#7f00ff]/30 to-[#e100ff]/30 backdrop-blur">
-                <div className="flex gap-2 pl-4">
-                  <PiTerminalBold className="text-[#311b64]" />
-                </div>
-                <div className="flex gap-2 pr-4">
-                  <PiWaveSineBold className="text-[#311b64]" />
-                  <PiHardDriveBold className="text-[#311b64]" />
-                  <PiAlienBold className="text-[#311b64]" />
-                </div>
-              </div>
-            )}
-            <img src={imgSrc} alt="Uploaded image preview" />
-            {imgFrame === 'youtube' && (
-              <div className="w-full absolute bottom-0">
-                <div className="flex flex-row w-full">
-                  <div className="w-full h-1 bg-danger" />
-                  <div className="w-full h-1 bg-gray-300" />
-                </div>
-                <div className="flex items-center justify-between  h-6 bg-[#000]/50">
+              style={{
+                borderRadius: `${borderRadius}px`,
+                transform: `scale(${
+                  imgScale * sliderScale
+                }) perspective(1000px) rotateX(${rotationX}deg) rotateY(${rotationY}deg) rotateZ(0deg)`,
+                boxShadow: `rgba(0, 0, 0, 0.6) 0px ${shadow}px ${
+                  shadow * 0.9
+                }px 0px`,
+              }}
+            >
+              {imgFrame === 'macOS-dark' && (
+                <div className="flex items-center justify-between w-full h-8 bg-[#262a35]">
                   <div className="flex gap-2 pl-4">
-                    <MdPause className="text-[#ffffff]" />
-                    <MdSkipNext className="text-[#ffffff]" />
-                    <MdVolumeUp className="text-[#ffffff]" />
+                    <div className="flex items-center justify-center w-3 h-3 bg-[#ff5f56] rounded-full"></div>
+                    <div className="flex items-center justify-center w-3 h-3 bg-[#ffbd2e] rounded-full"></div>
+                    <div className="flex items-center justify-center w-3 h-3 bg-[#27c93f] rounded-full"></div>
+                  </div>
+                </div>
+              )}
+              {imgFrame === 'macOS-light' && (
+                <div className="flex items-center justify-between w-full h-8 bg-[#f2f5f7]">
+                  <div className="flex gap-2 pl-4">
+                    <div className="flex items-center justify-center w-3 h-3 bg-[#ff5f56] rounded-full"></div>
+                    <div className="flex items-center justify-center w-3 h-3 bg-[#ffbd2e] rounded-full"></div>
+                    <div className="flex items-center justify-center w-3 h-3 bg-[#27c93f] rounded-full"></div>
+                  </div>
+                </div>
+              )}
+              {imgFrame === 'windows-light' && (
+                <div className="flex items-center justify-end w-full h-8 bg-[#d0dbec]/80 backdrop-blur">
+                  <div className="flex gap-2 pr-4">
+                    <MdMinimize className="text-[#313131]" />
+                    <MdOutlineRectangle className="text-[#313131]" />
+                    <MdClose className="text-[#313131]" />
+                  </div>
+                </div>
+              )}
+              {imgFrame === 'windows-dark' && (
+                <div className="flex items-center justify-end w-full h-8 bg-[#1e1e20]/80 backdrop-blur">
+                  <div className="flex gap-2 pr-4">
+                    <MdMinimize className="text-[#ffffff]" />
+                    <MdOutlineRectangle className="text-[#ffffff]" />
+                    <MdClose className="text-[#ffffff]" />
+                  </div>
+                </div>
+              )}
+              {imgFrame === 'galaxy' && (
+                // translucent navbar with a purple gradient background
+                <div className="flex items-center justify-between w-full h-8 bg-gradient-to-r from-[#7f00ff]/30 to-[#e100ff]/30 backdrop-blur">
+                  <div className="flex gap-2 pl-4">
+                    <PiTerminalBold className="text-[#311b64]" />
                   </div>
                   <div className="flex gap-2 pr-4">
-                    <MdClosedCaption className="text-[#ffffff]" />
-                    <MdSettings className="text-[#ffffff]" />
-                    <MdFullscreen className="text-[#ffffff]" />
+                    <PiWaveSineBold className="text-[#311b64]" />
+                    <PiHardDriveBold className="text-[#311b64]" />
+                    <PiAlienBold className="text-[#311b64]" />
                   </div>
                 </div>
+              )}
+              {/* <img src={imgSrc} alt="Uploaded image preview" /> */}
+              <div className="bg-content1/[.88] p-4 text-left overflow-hidden">
+                <Button onClick={formatCode}>Format</Button>
+                <ReactCodeMirror
+                  extensions={[loadLanguage('jsx')]}
+                  value={codeMirrorValue}
+                  theme={myTheme}
+                  minWidth="400px"
+                  minHeight="400px"
+                  height="100%"
+                  style={{ fontSize: '16px' }}
+                  basicSetup={{
+                    lineNumbers: false,
+                    foldGutter: false,
+                    highlightActiveLineGutter: false,
+                    highlightSpecialChars: false,
+                    allowMultipleSelections: false,
+                    highlightActiveLine: false,
+                    indentOnInput: true,
+                  }}
+                  onChange={onCMChange}
+                />
               </div>
-            )}
-          </div>
-        )}
+              {imgFrame === 'youtube' && (
+                <div className="w-full absolute bottom-0">
+                  <div className="flex flex-row w-full">
+                    <div className="w-full h-1 bg-danger" />
+                    <div className="w-full h-1 bg-gray-300" />
+                  </div>
+                  <div className="flex items-center justify-between  h-6 bg-[#000]/50">
+                    <div className="flex gap-2 pl-4">
+                      <MdPause className="text-[#ffffff]" />
+                      <MdSkipNext className="text-[#ffffff]" />
+                      <MdVolumeUp className="text-[#ffffff]" />
+                    </div>
+                    <div className="flex gap-2 pr-4">
+                      <MdClosedCaption className="text-[#ffffff]" />
+                      <MdSettings className="text-[#ffffff]" />
+                      <MdFullscreen className="text-[#ffffff]" />
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          )
+        }
       </div>
     )
   }
